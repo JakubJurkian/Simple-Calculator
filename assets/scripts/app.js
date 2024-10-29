bc3bfbbb1ad069ca6a7335409615aa39ee3ef2fb
Jakub <kuba.jur03@gmail.com>
2024-10-29 19:22:43 +0100

    added logic for displaying clicked numbers on the display

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,23 +4,88 @@ const input = document.querySelector('#field');
 const calcBtn = document.querySelector('.calculate-btn');
 const clearBtn = document.querySelector('.clear-btn');
 
-let firstNum = 0, secondNum = null;
-let typedFirstNum = "", typedSecondNum = "";
+let firstNum = 0,
+  secondNum = null;
+let typedFirstNum = '',
+  typedSecondNum = '';
 let operator, calcResult;
 
 const add = (n1, n2) => (n1 + n2).toFixed(2);
 const subst = (n1, n2) => (n1 - n2).toFixed(2);
 const power = (n1, n2) => (n1 * n2).toFixed(2);
 const divide = (n1, n2) => {
-  if (n2 == 0) return "Error";
-  return (n1 / n2).toFixed(2);
-}
+  if (n2 == 0) return 'Error';
+  else return (n1 / n2).toFixed(2);
+};
 
 function result(n1, n2, operator) {
   switch (operator) {
-    case "+": return add(n1, n2);
-    case "-": return subst(n1, n2);
-    case "*": return power(n1, n2);
-    case "/": return divide(n1, n2);
+    case '+':
+      return add(n1, n2);
+    case '-':
+      return subst(n1, n2);
+    case '*':
+      return power(n1, n2);
+    case '/':
+      return divide(n1, n2);
   }
+}
+
+function operatorClickedHandler(el) {
+  if (operator === el.textContent && !secondNum) {
+    if (secondNum === 0) return; //ocb
+    colorSign.classList.remove('sign-color');
+    operator = null;
+    return;
+  }
+
+  if (operator) return;
+
+  if (clearBtn.classList.contains('error-color')) {
+    clearBtn.classList.remove('error-color');
+  }
+
+  operator = el.textContent;
+  colorSign = el;
+  colorSign.classList.add('sign-color');
+}
+
+function numClickedHandler(el) {
+  if (operator) {
+    if (input.classList.contains('input-big-numbers')) {
+      input.classList.remove('input-big-numbers');
+    }
+    colorSign.classList.remove('sign-color');
+    typedSecondNum += el.textContent;
+    const typedSecondNumWithSpaces = numberWithSpaces(typedSecondNum);
+    input.value = typedSecondNumWithSpaces;
+    secondNum = parseInt(typedSecondNum);
+    // console.log(`sec num - ${secondNum}`);
+    calcBtn.disabled = false;
+    return;
+  }
+
+  typedFirstNum += el.textContent;
+  const typedFirstNumWithSpaces = numberWithSpaces(typedFirstNum);
+  input.value = typedFirstNumWithSpaces;
+  firstNum = parseInt(typedFirstNum);
+  calcBtn.disabled = true;
+
+  if (firstNum.toString().length < 15) {
+    input.classList.remove('input-big-numbers');
+  }
+}
+
+function numberWithSpaces(num) {
+  const parts = num.toString().split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  return parts.join('.');
+}
+
+for (const el of listOfNums) {
+  el.addEventListener('click', numClickedHandler.bind(null, el));
+}
+
+for (const el of listOfSigns) {
+  el.addEventListener('click', operatorClickedHandler.bind(null, el));
 }
